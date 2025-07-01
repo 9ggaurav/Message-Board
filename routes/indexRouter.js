57a50da55438c9cf21edea199e7cbecc2f69ad7d
Gaurav <zgaurav713@gmail.com>
2025-07-01 18:34:58 +0530

    show Asia time

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -10,7 +10,7 @@ indexRouter.get("/", (req, res) => {
 
 indexRouter.post("/", (req, res) => {
     const {author, message} = req.body;
-    messages.push({ id: nanoid(), text: message, user: author, added: new Date().toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'}), time: new Date().toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'}) });
+    messages.push({ id: nanoid(), text: message, user: author, added: new Date().toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric', timeZone: 'Asia/Kolkata'}), time: new Date().toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Kolkata'}) });
     res.redirect("/");
 })
 
